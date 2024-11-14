6b83e32706843c3c18f02eec29d811ca180a1e36
vinicius <vinicius.nascimentoliveira514@gmail.com>
2024-11-14 10:07:41 -0300

    Adicionado um botão de  Whatsapp na rolagem do site

--- menu.js
+++ menu.js
@@ -1,4 +1,7 @@
 window.addEventListener("scroll", function(){
     let header = document.querySelector('#header');
-    header.classList.toggle('rolagem', window.scrollY > 700)
+    header.classList.toggle('rolagem', window.scrollY > 1);
+
+    let botao = document.querySelector('.float');
+    botao.classList.toggle('rolagemWhats', window.scrollY > 0);
 })
