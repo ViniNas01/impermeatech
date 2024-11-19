2bce2414237673a0a4f3eaa7eca7839ac8a284df
vinicius <vinicius.nascimentoliveira514@gmail.com>
2024-11-19 17:10:45 -0300

    Site concluido com todos os links direcionados imagens definidas

--- menu.js
+++ menu.js
@@ -5,3 +5,17 @@ window.addEventListener("scroll", function(){
     let botao = document.querySelector('.float');
     botao.classList.toggle('rolagemWhats', window.scrollY > 0);
 })
+
+let btnMenu = document.getElementById('btn-menu');
+let menuMobile = document.getElementById('menu-mobile');
+let overlay = document.getElementById('overlay-menu');
+
+btnMenu.addEventListener('click', ()=>{
+    menuMobile.classList.add('abrir-menu');
+})
+menuMobile.addEventListener('click', ()=>{
+    menuMobile.classList.remove('abrir-menu');
+})
+overlay.addEventListener('click', ()=>{
+    menuMobile.classList.remove('abrir-menu');
+})
